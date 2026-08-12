f827ea2b6c16b0b77053c858ce08a2b0ab35d947
rey109 <reyza1098@gmail.com>
2026-08-12 12:19:53 +0700

    Fix allowedDevOrigins path in next.config.ts

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,9 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  experimental: {
-    allowedDevOrigins: ["172.16.0.2", "localhost:3000"],
-  },
+  allowedDevOrigins: ["172.16.0.2", "localhost:3000"],
 };
 
 export default nextConfig;
